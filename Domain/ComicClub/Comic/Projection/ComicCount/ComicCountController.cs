c51aca813ffb671e37a228fd50a57bda9fb2a2ed
Luis Pablo Galeas Bardales <galeaspablo@hotmail.com>
2025-01-28 12:38:32 +0000

    Begin controller for comic count projection

--- Domain/ComicClub/Comic/Projection/ComicCount/ComicCountController.cs
+++ Domain/ComicClub/Comic/Projection/ComicCount/ComicCountController.cs
@@ -1,6 +1,28 @@
+using EventSourcing.Common.Ambar;
+using EventSourcing.Common.Projection;
+using EventSourcing.Common.SerializedEvent;
+using Microsoft.AspNetCore.Mvc;
+
 namespace EventSourcing.Domain.ComicClub.Comic.Projection.ComicCount;
 
-public class ComicCountController
+[ApiController]
+[Route("api/v1/comic-club/comic/projection")]
+[Produces("application/json")]
+[Consumes("application/json")]
+[AmbarAuthMiddleware]
+public class ComicCountProjectionController : ProjectionController
 {
-    
+    private readonly ComicCountProjectionHandler _comicCountProjectionHandler;
+
+    public ComicCountProjectionController(
+        MongoTransactionalProjectionOperator mongoOperator,
+        Deserializer deserializer,
+        ILogger<ComicCountProjectionController> logger,
+        ComicCountProjectionHandler comicCountProjectionHandler)
+        : base(mongoOperator, deserializer, logger)
+    {
+        _comicCountProjectionHandler = comicCountProjectionHandler;
+    }
+
+    // expose the handler in an http endpoint
 }
